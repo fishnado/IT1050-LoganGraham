d64839cc7b02ab1c9e15dbaa87d79acbe7c0a87f
Logan_Graham <s00888563@acad.tri-c.edu>
2018-11-24 12:20:55 -0500

    1050Lab5 Final

---
+++ 1050Lab5/1050Lab5/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string Name { get; set; }
+
+        // Add bark() method
+
+        public string bark;
+        public static void bark();
+
+        {
+
+        Console.WriteLine( "{0} is Barking...",Name);
+
+        }
+
+    // Add doTrick() method
+    public string doTrick();
+    public string void doTrick(trickName);
+         {
+         Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", name, trickName);    
+         }
+    }
+}
